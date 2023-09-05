0414c78c9ae3fd5b7c20cb74de88f5a14aacd30b
Juan de Santos <juanj.desantos@gmail.com>
2023-09-05 10:06:33 +0200

    added images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '04.angular';
+export class AppComponent implements OnInit{
+  images = ['IMG_8554.jpeg', 'IMG_8670.jpeg', 'IMG_8844.jpeg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImages();
+  }
+
+  updateImages() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10)
+
+    }, 8000)
+  }
 }
